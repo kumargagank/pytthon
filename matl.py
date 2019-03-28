c10779074a6425d646eafec4f89043e52e397d59
kumargagank <48008085+kumargagank@users.noreply.github.com>
2019-03-28 15:08:44 +0530

    Add files via upload

---
+++ matl.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+#List of values x axis
+x=[2,3,4,5,6]
+y=[3,4,5,6,7]
+plt.plot(x,y)
+plt.show()
+
+plt.plot([2,3,4,5,6], [3,4,5,6,7], color="green") #green line
+plt.show()
